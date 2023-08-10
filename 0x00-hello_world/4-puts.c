ffbaf81394fddbda7c70a47571d0f605a7fbdeb7
Mary-Kiki <felishakinshan@gmail.com>
2023-08-10 14:16:01 -0700

    FirstC

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,16 @@
+/**
+ * File:4-puts.c
+ * Auth: Mary Kinyanjui
+ */
 #include <stdio.h>
+/**
+ * main - Prints "Programming is like building a multilingual puzzle,
+ *                    followed by a nwe line.
+ * Return: 0 always
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle,");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
