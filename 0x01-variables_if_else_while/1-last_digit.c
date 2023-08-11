879d5f4ffef25fde8d57c938c177601c9c3cf821
Mary-Kiki <felishakinshan@gmail.com>
2023-08-11 14:59:32 -0700

    Logic

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,17 +22,17 @@
 	if ((n % 10) > 5)
 	{
 		printf("Last digit of %d is %d and is greater than
-				5\n", n, n%10)
+				5\n", n, n%10);
 	}
 	else if ((n % 10) < 6 && != 10)
 	{
 		printf("Last digit of %d is %d and is less than 6
-				and not 0\n", n, n%10)
+				and not 0\n", n, n%10);
 	}
 	else 
 	{
 		printf("Last digit of %d is %d and is zero\n",
-				n, n%10)
+				n, n%10);
 	}
 	
 	return (0);
