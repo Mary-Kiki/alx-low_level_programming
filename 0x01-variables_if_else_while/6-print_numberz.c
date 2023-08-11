e104ec0c258e0034958c379203f189f6723f5938
Mary-Kiki <felishakinshan@gmail.com>
2023-08-11 16:29:31 -0700

    Integerput

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+/*
+ * File: 6-print_numberz.c
+ * Auth: Mary Kinyanjui
+ */
+
+#include <stdio.h>
+
+/**
+ * main -  Program that putchar single digits
+ *           of base 10 starting from 0
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+
+	putchar('\n');
+	return (0);
+}
+
