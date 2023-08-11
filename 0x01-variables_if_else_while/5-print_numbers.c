e1dffd9a11b68ccd68eb1d0fc1672f93267d5c54
Mary-Kiki <felishakinshan@gmail.com>
2023-08-11 16:14:21 -0700

    Forloop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * File: 5-print_numbers.c
+ * Auth: Mary Kinyanjui
+ */
+
+#include <stdio.h>
+
+/**
+ * main -  Program that prints single digits
+ *           of base 10 starting from 0
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
+
