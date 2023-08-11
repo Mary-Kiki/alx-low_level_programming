49873b171bff782811729a2b4fbce2b56c1bf905
Mary-Kiki <felishakinshan@gmail.com>
2023-08-11 15:27:40 -0700

    Putchar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: Mary Kinyanjui
+ */
+
+#include <stdio.h>
+
+/**
+ * main -  Program that prints alphabet in lower
+ *           case using putchar
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+	return (0);
+
+}
+
