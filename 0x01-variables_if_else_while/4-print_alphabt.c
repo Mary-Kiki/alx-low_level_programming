e482c22fff923e0e1a70873b2c37cd7566dd55ee
Mary-Kiki <felishakinshan@gmail.com>
2023-08-11 16:08:06 -0700

    while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -15,9 +15,9 @@ int main(void)
 {
 	char alphabet = 'a';
 
-	while ( alphabet <= 'z')
+	while (alphabet <= 'z')
 	{
-		if( alphabet != 'q' && alphabet != 'e')
+		if (alphabet != 'q' && alphabet != 'e')
 		{
 			putchar(alphabet);
 		}
