ab6d06666bb1fa8a6cb81c4ecf76b2147d27a15d
Mary-Kiki <felishakinshan@gmail.com>
2023-08-11 16:53:26 -0700

    hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,15 +14,14 @@
 int main(void)
 {
 	int i;
+	char letter;
 
 	for (i = 0; i < 10; i++)
 	{
 		putchar(i + '0');
 	}
 
-	char letter;
-
-	for (letter = 'a'; letter >= 'f'; letter++)
+	for (letter = 'a'; letter <= 'f'; letter++)
 	{
 		putchar(letter);
 	}
