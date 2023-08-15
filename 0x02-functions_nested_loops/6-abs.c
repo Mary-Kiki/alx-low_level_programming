eeeb42163f4bed7728cc9e499f15fed0f7bc3e45
Mary-Kiki <felishakinshan@gmail.com>
2023-08-15 16:58:29 -0700

    abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,14 +7,14 @@
 #include <stdlib.h>
 
 /**
- * int _abs - Program that prints
+ * _abs - Program that prints
  *              absolute number
  * @n: Character to be checked
  * Return: Always 0.
  */
 int _abs(int n)
 {
-	if ( n >= 0)
+	if (n >= 0)
 	{
 		return (n);
 	}
