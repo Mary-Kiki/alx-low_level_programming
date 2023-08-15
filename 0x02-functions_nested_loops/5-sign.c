f7d5df3d5d5e81270ec84b8b323f669513b18537
Mary-Kiki <felishakinshan@gmail.com>
2023-08-15 16:41:05 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,33 @@
+/*
+ * File: 5-sign.c
+ * Auth: Mary Kinyanjui
+ */
+
+#include "main.h"
+
+/**
+ * print_sign - Program that prints sign
+ *              after conditions
+ * @n - Character to be checked
+ * Return: Always 0.
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if ( n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+
+}
+
