82d5ad260e19bf27fd53c587aa79fe07886e0338
Mary-Kiki <felishakinshan@gmail.com>
2023-08-15 13:09:47 -0700

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+/*
+ * File: 0-putchar.c
+ * Auth: Mary Kinyanjui
+ */
+
+#include <stdio.h>
+
+/**
+ * main -  Program that putchar _putchar
+ *           followed new line
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	putchar('_putchar\n');
+
+	return (0);
+}
+
