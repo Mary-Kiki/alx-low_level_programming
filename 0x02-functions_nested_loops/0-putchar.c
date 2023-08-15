825b5499b7226a34fb8896992180b51a3ef999a3
Mary-Kiki <felishakinshan@gmail.com>
2023-08-15 13:24:23 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
  * Auth: Mary Kinyanjui
  */
 
-#include <main.h>
+#include "main.h"
 
 /**
  * main -  Program that putchar _putchar
@@ -13,7 +13,15 @@
  */
 int main(void)
 {
-	putchar('_putchar\n');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
