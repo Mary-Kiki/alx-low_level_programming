1c4a1d4fa76999adb40f112928924a3ef2d05be7
Mary-Kiki <felishakinshan@gmail.com>
2023-08-15 16:22:28 -0700

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,27 @@
+/*
+ * File: 4-isalpha.c
+ * Auth: Mary Kinyanjui
+ */
+
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _isalpha -  Program that putchar 1
+ *               if c is lowercase
+ * @c - Character to be checked
+ * Return: Always 0.
+ */
+int _isalpha(int c)
+{
+	if (isalpha(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+}
+
