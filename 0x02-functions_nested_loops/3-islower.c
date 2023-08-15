4d220b7d62c927bb3240bcce554822966f3e2dd4
Mary-Kiki <felishakinshan@gmail.com>
2023-08-15 16:07:03 -0700

    Tria

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,26 @@
+/*
+ * File: 3-islower.c
+ * Auth: Mary Kinyanjui
+ */
+
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * _islower -  Program that putchar 1
+ *               if c is lowercase
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
