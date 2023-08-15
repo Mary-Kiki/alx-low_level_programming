23b45b29479598c8e1d12cb1267c07645ed6c07c
Mary-Kiki <felishakinshan@gmail.com>
2023-08-15 14:48:24 -0700

    Two_nested-loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: Mary Kinyanjui
+ */
+
+#include "main.h"
+
+/**
+ * print_alphabet_x10 -  Program that putchar alphabet
+ *                         in lowercase 10
+ *
+ * Return: Always 0.
+ */
+void print_alphabet_x10(void)
+{
+	int times = 0;
+	
+	while (times < 10)
+	{
+		char letter = 'a';
+
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+	}
+	_putchar('\n');
+}
+
