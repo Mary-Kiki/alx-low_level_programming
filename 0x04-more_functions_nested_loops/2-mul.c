a061526f1ea36fadd6836ec26d2143ec7a4ae899
Mary-Kiki <felishakinshan@gmail.com>
2023-08-17 13:55:08 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,23 @@
+/*
+ * File: 2-mul.c
+ * Auth: Mary Kinyanjui
+ */
+
+#include "main.h"
+
+/**
+ *  mul-Program that multiply
+ *          2 digit
+ *
+ * @c: Parameter to be checked.
+ *
+ * @b: Paremeter to be checked.
+ *
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
+
