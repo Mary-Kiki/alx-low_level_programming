97b653d7a3003e823fc7fd9715dde3f0a41c22cf
Mary-Kiki <felishakinshan@gmail.com>
2023-08-21 19:31:46 -0700

    Length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,25 @@
+/*
+ * File: 2-strlen.c
+ * Auth: Mary Kinyanjui
+ */
+
+#include "main.h"
+
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int c = 0;
+
+	for (; *s != '\0'; s++)
+	{
+		c++;
+	}
+
+	return (c);
+}
+
