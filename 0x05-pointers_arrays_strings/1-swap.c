074b3eed88e093c84a09e7cf1d99a38714666507
Mary-Kiki <felishakinshan@gmail.com>
2023-08-21 19:18:40 -0700

    Battery st.

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+/*
+ * File: 1-swap.c
+ * Auth: Mary Kinyanjui
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value of pointer
+ *                 to 98
+ *
+ * @n: Pointer to an int
+ * Return: Always 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
