5917b6f860610a443ba6e87bc296b4dc332d69af
Mary-Kiki <felishakinshan@gmail.com>
2023-08-24 13:10:32 -0700

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,25 @@
+/**
+ * *_strncat - Concanate two strings
+ *              using strncat
+ *
+ * @src: String to concanate.
+ * @dest: String to concanate.
+ * @n: max num of chars.
+ *
+ * Return: pointer=dest.
+ */
+
+#include "main.h"
+#include <string.h>
+
+char *_strncat(char *dest, char *src, int n)
+{
+	char *ptr;
+
+	strncat(dest, src, n);
+
+	ptr = dest;
+
+	return(ptr);
+}
+
