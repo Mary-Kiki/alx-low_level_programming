afcb781de209aa4c1149120409eaf135b6035521
Mary-Kiki <felishakinshan@gmail.com>
2023-08-24 13:51:04 -0700

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+/**
+ * _strcmp - Compares two
+ *             strings
+ *
+ * @s1: String to concanate.
+ * @s2: String to concanate.
+ *
+ * Return: zero
+ */
+
+#include "main.h"
+#include <string.h>
+
+int _strcmp(char *s1, char *s2)
+{
+
+	return strcmp(s1, s2);
+}
+
